501d64e5d4580279ba98a0bc89d86d46575b058f
Hunter101H0 <100170708+Hunter101H0@users.noreply.github.com>
2022-08-21 04:10:42 +0300

    2.11.Slider

--- js/index.js
+++ js/index.js
@@ -31,57 +31,40 @@ menu = document.querySelector("header .menu"),
 }  
 // ===
 
-// Get Slider Itme | Array.from [ES6 Feature]
 let sliderImges = Array.from(document.querySelectorAll('.slider-Container img'));
 console.table(sliderImges);
 
-// Get Number of Slide
 let slidesCount = sliderImges.length;
 
-// set Current Slide
 let currentSlide = 1;
 
-// Slide Number Element
 let slideNumberElement = document.getElementById("slide-number");
 
-// Previous and Next Buttons && prev Buttons
 let nextButton = document.getElementById('next');
 let prevButton = document.getElementById('prev');
 
-// Handle Click on Previous and Next Buttons
 nextButton.onclick = nextSlide;
 prevButton.onclick = prevSlide;
 
-// create The UL Element
 let pagiationElement = document.createElement("ul");
-// Set ID On Created UL Element
 pagiationElement.setAttribute("id", "pagiation-ul");
 
-// Create List Items Based On Slides Count
 for (var i = 1; i <= slidesCount; i++) {
-    // Create The LI
     let pagiationItem = document.createElement("li");
 
-    // Set Custom Attribute
     pagiationItem.setAttribute("data-index", i);
 
-    // Set Item Content
     pagiationItem.appendChild(document.createTextNode(i));
 
-    // Append Items to The Main Ul List
     pagiationElement.appendChild(pagiationItem);
 }
 
-// Add The Creatd Ul Element to The page
 document.getElementById("indicators").appendChild(pagiationElement)
 
-// Get The New Created Ul
 let pagiationCreatedUl = document.getElementById("pagiation-ul");
 
-// Get pagiations Itme | Array.from [ES6 Feature]
 let pagiationsBullets = Array.from(document.querySelectorAll('#pagiation-ul li'));
 
-// Loop Through All Bullets Items
 for (let i = 0; i < pagiationsBullets.length; i++) {
     pagiationsBullets[i].onclick = function () {
         currentSlide = parseInt(this.getAttribute('data-index'));
@@ -89,23 +72,18 @@ for (let i = 0; i < pagiationsBullets.length; i++) {
     }
 }
 
-// Trigger The Checker Function
 theChecker();
 
-// Next Slide Function
 function nextSlide() {
     if (nextButton.classList.contains('disabled')) {
-        // Do Nothing
         return false;
     }else {
         currentSlide++;
         theChecker();
     }
 }
-// previous Slide Function
 function prevSlide() {
     if (prevButton.classList.contains('disabled')) {
-        // Do Nothing
         return false;
     }else {
         currentSlide--;
@@ -114,44 +92,31 @@ function prevSlide() {
 }
 
 function theChecker() {
-    // Set The Slide Number
     slideNumberElement.textContent = 'Slide #' + (currentSlide) + ' of ' + (slidesCount);
 
-    // Remove All Active Classes
     removeAllActive();
 
-    // Set Active Class On Current Slide
     sliderImges[currentSlide - 1].classList.add("active");
 
-    // Set Active Class On Current Pagination Item
     pagiationCreatedUl.children[currentSlide - 1].classList.add("active");
 
-    // Check if Current Slide is The First 
     if (currentSlide == 1) {
-        // Add disabled Class on Previous Button
         prevButton.classList.add("disabled");
     } else {
-        // Remove disabled Class From Previous Button
         prevButton.classList.remove("disabled");
     }
 
-    // Check if Current Slide is The Last 
     if (currentSlide == slidesCount) {
-        // Add disabled Class on next Button
         nextButton.classList.add("disabled");
     } else {
-        // Remove disabled Class From next Button
         nextButton.classList.remove("disabled");
     }
 }
 
-// Remove All Active Class From imges and Pagination Bullests
 function removeAllActive() {
-    // Loop Through Imges
     sliderImges.forEach(function(img) {
         img.classList.remove("active");
     });
-    // Loop Therough pagiation Bullets
     pagiationsBullets.forEach(function (bullets) {
         bullets.classList.remove("active");
     });
